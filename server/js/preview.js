49a646f10f97d6bebcac07b5d8029c6ac575ea19
firoh86 <firohh.as@gmail.com>
2019-06-01 21:20:39 +0200

    terminado constructor de preview

--- server/js/preview.js
+++ server/js/preview.js
@@ -11,32 +11,47 @@ const sectionTitleButton = document.getElementById('section-title-button')
 const sectionParagraphButton = document.getElementById('section-paragraph-button')
 const codeParagraphButton = document.getElementById('code-paragraph-button')
 
+
+//constructor de pagina para subir a las bases de datos
+const finalTitle = []
+const sectionTitles = []
+const completDocument = []
+
+
+
 if (form) {
     form.addEventListener('submit', (e) => {
         e.preventDefault()
+        console.log('titulo final =' + finalTitle);
+        if (sectionTitles.length > 0) console.log('subtitulos de seccion =' + sectionTitles);
+        console.log('documento completo =' + completDocument);
     })
     titleButton.addEventListener('click', (e) => {
 
         preview.innerHTML = `<h1>${title.value}</h1>`
+        completDocument[0] = `<h1>${title.value}</h1>`
+        finalTitle[0] = `<h1>${title.value}</h1>`
         title.value = ''
+
     })
     sectionTitleButton.addEventListener('click', (e) => {
         preview.innerHTML += `<h2>${sectionTitle.value}</h2>`
+        sectionTitles.push(`<h2>${sectionTitle.value}</h2>`)
+        completDocument.push(`<h2>${sectionTitle.value}</h2>`)
         sectionTitle.value = ''
     })
     sectionParagraphButton.addEventListener('click', (e) => {
         preview.innerHTML += `<p>${sectionParagraph.value}</p>`
+        completDocument.push(`<p>${sectionParagraph.value}</p>`)
         sectionParagraph.value = ''
     })
 
     codeParagraphButton.addEventListener('click', (e) => {
         preview.innerHTML += `<pre><code class='language-css'>${codeParagraph.value}</code></pre>`
+        completDocument.push(`<pre><code class='language-css'>${codeParagraph.value}</code></pre>`)
         codeParagraph.value = ''
     })
 
-    const saveData = () => {
-
-    }
 }
 
 
